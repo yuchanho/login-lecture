be3e5129312c799271b6e9f2a5bc9dc2511c25e7
yuchanho <yj127812@naver.com>
2023-01-15 14:45:18 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+const home = require("./src/routes/home");
+app.use("/", home); // use -> 미들웨어를 등록해주는 매서드
+
+module.exports = app;
