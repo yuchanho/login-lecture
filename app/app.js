288d1765ba2dc1f550e4162e4b177fba4bb2e66c
yuchanho <yj127812@naver.com>
2023-01-15 14:55:10 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ const app = express();
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 const home = require("./src/routes/home");
 app.use("/", home); // use -> 미들웨어를 등록해주는 매서드
